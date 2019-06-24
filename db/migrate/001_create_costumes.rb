402eecdff27c8041db482c699109b81dfb8fe8cf
Travis Exline <t.exline22@gmail.com>
2019-06-24 14:48:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,9 +11,11 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.float :price
       t.string :size
       t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
     end
   end
 end
