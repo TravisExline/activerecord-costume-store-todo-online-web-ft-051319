859fa0d5a7dbf62359246dd82bac84ec59cdc385
Travis Exline <t.exline22@gmail.com>
2019-06-24 14:36:45 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,12 +4,14 @@ class CreateHauntedHouses < ActiveRecord::Migrations[5.1]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.string :theme
       t.float :price
+      t.string :theme
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string :description
+      t.text :description
+
+      t.timestamps null: false
     end
   end
 end
